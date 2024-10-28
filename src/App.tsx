12d8b410085b30cb86d4df6c4e478d3a166abcac
f95777yw <yat.wong-4@student.manchester.ac.uk>
2024-10-28 21:11:30 +0000

    pass data to components using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import ListGroup from './components/ListGroup';
 
 function App(){
+  let items = ["Satoru", "Suguru", "Hi"];
   // equivalent to <div><Message></Message></div>
-  return <div><ListGroup /></div>
+  return <div><ListGroup items={items} heading='goats'/></div>
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,45 @@
-function ListGroup() {
-  const items = ["Satoru", "Suguru", "Hi"];
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({items, heading}: Props) {
+
+
+  // const errorMessage = () => {
+  //   return items.length === 0 && <p>No items found</p>;;
+  // };
+
+  // This is an event handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
+  // State hook, tells react this component have data or state that changes over time
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     // Tell react to wrap it in fragment
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {/* {errorMessage} */}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
